512f4ed4952e279a28d94878914a00f860ab888e
JasperStFun <kosto55@mail.ru>
2021-12-27 12:18:15 +0300

    WTForms

--- flsite.py
+++ flsite.py
@@ -8,6 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from FDataBase import FDataBase
 from UserLogin import UserLogin
+from forms import LoginForm, RegisterForm
 
 
 DATABASE = '/tmp/flsite.db'
@@ -115,15 +116,16 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('profile'))
 
-    if request.method == 'POST':
-        user = dbase.getUserByEmail(request.form['email'])
-        if user and check_password_hash(user['psw'], request.form['psw']):
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = dbase.getUserByEmail(form.email.data)
+        if user and check_password_hash(user['psw'], form.psw.data):
             userlogin = UserLogin().create(user)
-            rm = True if request.form.get('remainme') else False
+            rm = form.remember.data
             login_user(userlogin, remember=rm)
             return redirect(request.args.get('next') or url_for('profile'))
         flash('Неверный логин или пароль', 'error')
-    return render_template('login.html', menu=dbase.getMenu(), title='Авторизация')
+    return render_template('login.html', menu=dbase.getMenu(), title='Авторизация', form=form)
 
 
 @app.route('/logout')
@@ -136,19 +138,16 @@ def logout():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    if request.method == 'POST':
-        if len(request.form['name']) > 4 and len(request.form['email']) > 4 \
-            and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
-            hash = generate_password_hash(request.form['psw'])
-            res = dbase.addUser(request.form['name'], request.form['email'], hash)
-            if res:
-                flash('Вы успешно зарегистрировались', 'success')
-                return redirect(url_for('login'))
-            else:
-                flash('Ошибка при добавлении в БД', 'error')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hash = generate_password_hash(form.psw.data)
+        res = dbase.addUser(form.name.data, form.email.data, hash)
+        if res:
+            flash('Вы успешно зарегистрировались', 'success')
+            return redirect(url_for('login'))
         else:
-            flash('Неверно заполнены поля', 'error')
-    return render_template('register.html', menu=dbase.getMenu(), title='Регистрация')
+            flash('Ошибка при добавлении в БД', 'error')
+    return render_template('register.html', menu=dbase.getMenu(), title='Регистрация', form=form)
 
 
 @app.route('/userava')
